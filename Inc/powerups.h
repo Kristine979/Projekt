1fcbfc59e13af4fb9ba32a5ff25cd0ac0337ba06
Kristine979 <s234473@dtu.dk>
2026-01-16 12:32:28 +0100

    powerups

---
+++ Inc/powerups.h
@@ -0,0 +1,34 @@
+/*
+ * powerups.h
+ *
+ *  Created on: 16. jan. 2026
+ *      Author: Bruger
+ */
+
+#ifndef POWERUPS_H_
+#define POWERUPS_H_
+#include "defs.h"
+#include "ship.h"
+#define MULTIPLEBULLETS 0
+#define DOUBLEDAMAGE 1
+#define FASTERBULLETS 2
+#define GAINLIFE 3
+
+/* Powerups:
+ * Multiple bullets (5)
+ * double damage
+ * faster bullets
+ * gain life
+ */
+
+typedef struct {
+int x, y, power, alive;
+} power_up_t;
+
+int did_ship_hit_power_up(power_up_t pu, ship_coord_t sc, ship_size_t ss);
+
+void spawn_power_up(power_up_t *pu);
+
+void move_power_up(power_up_t *pu, power_up_t pu_check, ship_coord_t sc, ship_size_t ss);
+
+#endif /* POWERUPS_H_ */
