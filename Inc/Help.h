544561b04aba5943021a57e1369a4bc6c2763cb6
YuriiPavlenko1 <s255795@dtu.dk>
2026-01-16 14:49:20 +0100

    Help

---
+++ Inc/Help.h
@@ -0,0 +1,15 @@
+/*
+ * Help.h
+ *
+ *  Created on: 16 Jan 2026
+ *      Author: root
+ */
+
+#ifndef HELP_H_
+#define HELP_H_
+#include "menu.h"
+
+void help();
+void PrintTextHelp(int x1, int y1, int x2, int y2, const char text[]);
+
+#endif /* HELP_H_ */
