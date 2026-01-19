8d561988035c0503ed39bc2e63ed44ce972bbc4a
YuriiPavlenko1 <s255795@dtu.dk>
2026-01-19 15:13:04 +0100

    Death Screen

---
+++ Inc/DeathScreen.h
@@ -0,0 +1,16 @@
+/*
+ * DeathScreen.h
+ *
+ *  Created on: 19 Jan 2026
+ *      Author: root
+ */
+
+#ifndef DEATHSCREEN_H_
+#define DEATHSCREEN_H_
+
+#include "menu.h"
+
+void Death();
+
+
+#endif /* DEATHSCREEN_H_ */
